02f3043e70a4f188495fcf1b90fcb96b5d026039
Shreeallabh Tripathi <157093445+Vallabh0440@users.noreply.github.com>
2025-01-08 15:48:05 +0530

    Add files via upload
    08

---
+++ 08_problem.js
@@ -0,0 +1,73 @@
+// You and your friends Tom, Jane, Peter and John got their final exam results. Your total score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56 and John’s total score is 40. The grading chart is 80 or above A grade 60 or above B grade 50 or above C grade 40 or above D grade 39 or less => F grade
+
+// Write a program to find your and your friends’ grades using if-else.
+
+let vallabhScore = 89;
+let TomScore = 66;
+let JaneScore = 95;
+let PeterScore = 56;
+let JohnScore = 40;
+
+if(vallabhScore >= 80 ){
+    console.log("vallabh got A grade")
+}else if(vallabhScore >=60 && vallabhScore <=80 ){
+    console.log("you got B grade")
+}else if(vallabhScore >= 50 && vallabhScore <= 60){
+    console.log("you got C grade")
+}else if(vallabhScore >=40 && vallabhScore <= 50){
+    console.log("you got D grade")
+}else if(vallabhScore ==  39 && vallabhScore <= 39){
+    console.log("you are failllllllll ")
+}
+
+
+if(TomScore >= 80 ){
+    console.log("you got A grade")
+}else if(TomScore >=60 && TomScore <=80 ){
+    console.log("Tom got B grade")
+}else if(TomScore >= 50 && TomScore <= 60){
+    console.log("you got C grade")
+}else if(TomScore >=40 && TomScore <= 50){
+    console.log("you got D grade")
+}else if(TomScore ==  39 && TomScore <= 39){
+    console.log("you are failllllllll ")
+}
+
+
+
+if(JaneScore >= 80 ){
+    console.log("Jane got A grade")
+}else if(JaneScore >=60 && JaneScore <=80 ){
+    console.log("Tom got B grade")
+}else if(JaneScore >= 50 && JaneScore <= 60){
+    console.log("you got C grade")
+}else if(JaneScore >=40 && JaneScore <= 50){
+    console.log("you got D grade")
+}else if(JaneScore ==  39 && JaneScore <= 39){
+    console.log("you are failllllllll ")
+}
+
+
+if(PeterScore >= 80 ){
+    console.log("Jane got A grade")
+}else if(PeterScore >=60 && PeterScore <=80 ){
+    console.log("Tom got B grade")
+}else if(PeterScore >= 50 && PeterScore <= 60){
+    console.log("Peter got C grade")
+}else if(PeterScore >=40 && PeterScore <= 50){
+    console.log("you got D grade")
+}else if(PeterScore ==  39 && PeterScore <= 39){
+    console.log("you are failllllllll ")
+}
+
+if(JohnScore >= 80 ){
+    console.log("vallabh got A grade")
+}else if(JohnScore >=60 && JohnScore <=80 ){
+    console.log("you got B grade")
+}else if(JohnScore >= 50 && JohnScore <= 60){
+    console.log("you got C grade")
+}else if(JohnScore >=40 && JohnScore <= 50){
+    console.log("John got D grade")
+}else if(JohnScore ==  39 && JohnScore <= 39){
+    console.log("you are failllllllll ")
+}
